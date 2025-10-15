a3e0482b7f2f44b7c89a3dca60048f77ebb9fda3
Bhargav-sunil <bhargavsunil2166@gmail.com>
2025-10-15 21:06:19 +0530

    Update usersRoutes.js

--- Server/routes/usersRoutes.js
+++ Server/routes/usersRoutes.js
@@ -1,19 +1,15 @@
-const express = require("express");
-const {
-  getAllUsers,
-  createUser,
-  updateUser,
-  deleteUser,
-} = require("../controllers/userController");
-const { auth, adminAuth } = require("../middleware/auth");
+const express = require('express');
+const { getAllUsers, createUser, updateUser, deleteUser } = require('../controllers/userController');
+const { auth, adminAuth } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.use(auth);
 
-router.get("/", getAllUsers);
-router.post("/", adminAuth, createUser);
-router.put("/:id", adminAuth, updateUser);
-router.delete("/:id", adminAuth, deleteUser);
+router.get('/', getAllUsers);
+
+router.post('/', adminAuth, createUser);
+router.put('/:id', adminAuth, updateUser);
+router.delete('/:id', adminAuth, deleteUser);
 
 module.exports = router;
